d63098e1bd07725c6f0c74b96442d1db76e77015
Matan <matantoled@gmail.com>
2025-07-23 00:17:11 +0300

    adding output

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,8 +8,10 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>(); 
 
   // avatar = input.required<string>();
   // name = input.required<string>();
@@ -23,5 +25,6 @@ export class UserComponent {
   // });
 
   onSelectUser() {
+    this.select.emit(this.id);
   }
 }
